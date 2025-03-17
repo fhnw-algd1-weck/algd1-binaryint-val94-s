5d0ded90d6a5cf2c6d012f7b973f01285cdb74df
val94-s <valerie.scheidegger1@students.fhnw.ch>
2025-03-17 13:32:22 +0100

    BinConverter Ergänzung

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -4,12 +4,30 @@ public class BinConverter {
 	public static String toString(int x) {
 		// TODO expect x to be in range [-128, 127], return string with 8 binary
 		// digits representing x in 2-complement
+
+		if(x < -128 || x > 127){
+			throw new IllegalArgumentException();
+		}
+
+
+
+
 		return "00000000";
 	}
 
 	public static int fromString(String text) {
 		// TODO expect text to contain 8 binary digits, parse to int value in
 		// 2-complement
-		return 0;
+		if(text.length() != 8 || !text.matches("[01]+")){
+			throw new IllegalArgumentException();
+		}
+
+		int value = Integer.parseInt(text, 2);
+
+		if(value > 127){
+			return value - 256;
+		}
+
+		return value;
 	}
 }
